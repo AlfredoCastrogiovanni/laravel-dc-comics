8b2bbd77674deeb6d65baaa075fdfc68eda44060
Alfredo Castrogiovanni <castro.alfry@gmail.com>
2024-02-05 16:46:03 +0100

    project done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title','Laravel App Title')</title>
+        <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         @vite('resources/js/app.js')
 
         @yield('head-content')
